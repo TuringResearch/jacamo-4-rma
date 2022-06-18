91f1cc20c341def6db045555111798794758cc4c
Fabian Brandão <fabian.brandao@nubank.com.br>
2022-06-18 01:09:33 -0300

    Testing.

--- bin/install_maven_dependencies.bat
+++ bin/install_maven_dependencies.bat
@@ -1,3 +1,3 @@
 call mvn install:install-file -Dfile=../lib/jason-2.4.jar -DgroupId=jason-lang -DartifactId=jason -Dversion=2.4 -Dpackaging=jar -DgeneratePom=true
 call mvn install:install-file -Dfile=../lib/cartago-2.4.jar -DgroupId=CArtAgO-lang -DartifactId=cartago -Dversion=2.4 -Dpackaging=jar -DgeneratePom=true
-call mvn install:install-file -Dfile=../lib/javino_stable_1.1.jar -DgroupId=br.pro.turing -DartifactId=javino_stable -Dversion=1.1 -Dpackaging=jar -DgeneratePom=true
+call mvn install:install-file -Dfile=../lib/javino_stable_1.2.jar -DgroupId=br.pro.turing -DartifactId=javino_stable -Dversion=1.1 -Dpackaging=jar -DgeneratePom=true
