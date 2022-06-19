a01197b458f5a7cb97304e2d1568068b83ecfa4a
Fabian Brandão <fabian.brandao@nubank.com.br>
2022-06-19 01:30:01 -0300

    Testing.

--- bin/install_maven_dependencies.sh
+++ bin/install_maven_dependencies.sh
@@ -2,3 +2,4 @@
 mvn install:install-file -Dfile=../lib/jason-2.4.jar -DgroupId=jason-lang -DartifactId=jason -Dversion=2.4 -Dpackaging=jar -DgeneratePom=true
 mvn install:install-file -Dfile=../lib/cartago-2.4.jar -DgroupId=CArtAgO-lang -DartifactId=cartago -Dversion=2.4 -Dpackaging=jar -DgeneratePom=true
 mvn install:install-file -Dfile=../lib/javino_stable_1.2.jar -DgroupId=br.pro.turing -DartifactId=javino_stable -Dversion=1.2 -Dpackaging=jar -DgeneratePom=true
+mvn install:install-file -Dfile=../lib/contextnet-2.7-eSMA.jar -DgroupId=br.pro.turing -DartifactId=javino_stable -Dversion=2.7 -Dpackaging=jar -DgeneratePom=true
